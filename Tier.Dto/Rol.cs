0f29ea3e3f0e5691a94ecb416b481df93110dfc3
Rjaspe0905 <csd.ricardo@gmail.com>
2015-07-19 14:05:59 -0500

    Se agregan clas clases para el mapeo de entidades de bases de dato en DTO.

---
+++ Tier.Dto/Rol.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tier.Dto
+{
+    public class Rol
+    {
+        public Nullable<Int16> idrol { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public Nullable<bool> activo { get; set; }
+        public Nullable<DateTime> fechacreacion { get; set; }
+    }
+}
