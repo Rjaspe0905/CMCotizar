d95dbb59ac0fd7b552faab6b394cf34ef6240b94
Rjaspe0905 <csd.ricardo@gmail.com>
2015-07-19 20:05:17 -0500

    Se escribe la programacion base para el acceso a datos.

--- Tier.Business/BUsuario.cs
+++ Tier.Business/BUsuario.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Tier.Business
 {
-    public class ParentBusiness
-    {
+    public class BUsuario : ParentBusiness
+    {       
     }
 }
--- Tier.Services/CMCotizar.cs
+++ Tier.Services/CMCotizar.cs
@@ -9,6 +9,6 @@ namespace Tier.Services
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CMCotizar" in both code and config file together.
     public class CMCotizar : ICMCotizar
-    {
+    {       
     }
 }
